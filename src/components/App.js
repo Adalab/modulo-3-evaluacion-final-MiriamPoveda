8160ad983d84e1b08e77f3f4ea5582596e430bdb
MiriamPoveda <mpoveda94@gmail.com>
2022-05-06 13:23:02 +0200

    Input correction

--- src/components/App.js
+++ src/components/App.js
@@ -124,7 +124,7 @@ function App() {
         handleRecharge={handleRecharge}
         handleFilterFilm={handleFilterFilm}
         handleFilterYear={handleFilterYear}
-        films={allFilters}
+        filteredFilm={filteredFilm}
         getYears={getYears()}
         />
         {incorrectName()}
--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -6,7 +6,7 @@ function Filters(props) {
   return (
     <section>
       <form onSubmit={props.handleRecharge}>
-          <FilterMovie handleFilterFilm={props.handleFilterFilm}/>
+          <FilterMovie handleFilterFilm={props.handleFilterFilm} filteredFilm={props.filteredFilm}/>
           <FilterYear 
           getYears={props.getYears}
           handleFilterYear={props.handleFilterYear}/>
