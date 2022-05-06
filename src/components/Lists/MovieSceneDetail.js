70eb12573289b172c61ccc6a616d92523231a603
MiriamPoveda <mpoveda94@gmail.com>
2022-05-06 16:37:46 +0200

    Revisiting routes

--- src/components/Lists/MovieSceneDetail.js
+++ src/components/Lists/MovieSceneDetail.js
@@ -1,11 +1,17 @@
 import {Link} from 'react-router-dom';
+import IncorrectPage from '../Notices/IncorrectPage';
 
 function MovieSceneDetail(props) {
 
+  /* Me permite poner en el navegador la ruta con /movie/ o sin ello */
+  if (props.oneFilm === undefined) {
+    return <IncorrectPage />;
+  }
+
     return (
-        <section>
+        <section key={props.oneFilm.id}>
         <div>
-          <Link to="/">Back</Link>
+          <Link to={"/"}>Back</Link>
           </div>
         <img
           className="filmPoster" 
@@ -15,7 +21,7 @@ function MovieSceneDetail(props) {
           <p>{props.oneFilm.name}</p>
           <p>{props.oneFilm.quote}</p>
           <p>Director: {props.oneFilm.director}</p>
-          <a href={props.oneFilm.audio} targe="blank">¡Listen to the quote!</a>
+          <a href={props.oneFilm.audio} target="blank">¡Listen to the quote!</a>
           </section>
     );
   }
